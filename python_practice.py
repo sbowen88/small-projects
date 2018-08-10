eca3670a91487f4c721653a59daac2bd4fb53a74
sbowen88 <sbowey@gmail.com>
2018-08-10 14:18:41 -0600

    for loop with dictionary

--- python_practice.py
+++ python_practice.py
@@ -2,3 +2,14 @@ names = ["Adam","Alex","Mariah","Martine","Columbus"]
 
 for name in names: 
   print name
+
+  webster = {
+  "Aardvark" : "A star of a popular children's cartoon show.",
+  "Baa" : "The sound a goat makes.",
+  "Carpet": "Goes on the floor.",
+  "Dab": "A small amount."
+}
+
+# Add your code below!
+for key in webster: 
+  print webster[key]
