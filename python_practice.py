40359b29f527c13fddc39d29a3b42e227a635b37
sbowen88 <sbowey@gmail.com>
2018-08-11 11:02:09 -0600

    conditions in for loops

--- python_practice.py
+++ python_practice.py
@@ -13,3 +13,10 @@ for name in names:
 # Add your code below!
 for key in webster: 
   print webster[key]
+
+#indentation for conditions in for loops
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+for item in a: 
+  if item % 2 == 0:
+  	print item 
