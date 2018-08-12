334ff1f072fc0c7609498ade5aca14316bdc1414
sbowen88 <sbowey@gmail.com>
2018-08-12 12:12:37 -0600

    functions in python

--- python_practice.py
+++ python_practice.py
@@ -20,3 +20,11 @@ a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 for item in a: 
   if item % 2 == 0:
   	print item 
+
+# Write your function below!
+def fizz_count(x):
+  count = 0
+  for item in x: 
+    if item == "fizz":
+      count +=1
+  return count
