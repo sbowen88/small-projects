79e889793ed2e847e0dd9086bac2cc65a259f170
sbowen88 <sbowey@gmail.com>
2018-08-15 15:02:51 -0700

    click event

--- jquery_practice.js
+++ jquery_practice.js
@@ -1,3 +1,14 @@
 $('.login-button').on('click', () => {
     $('.login-form').toggle();
   });
+
+//Event handlers
+$(document).ready(() => {
+    const $menuButton = $('.menu-button');
+    const $navDropdown = $('#nav-dropdown');
+  $menuButton.on('click', ()=>{
+    $navDropdown.show()
+  })
+  
+  
+  })
