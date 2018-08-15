53c5828495367fb36067b6388d48bac428bd4568
sbowen88 <sbowey@gmail.com>
2018-08-14 18:04:30 -0700

    looping through strings

--- python_practice.py
+++ python_practice.py
@@ -28,3 +28,18 @@ def fizz_count(x):
     if item == "fizz":
       count +=1
   return count
+
+  #looping through strings
+  for letter in "Codecademy":
+  print letter
+    
+# Empty lines to make the output pretty
+print
+print
+
+word = "Programming is fun!"
+
+for letter in word:
+  # Only print out the letter i
+  if letter == "i":
+    print letter
