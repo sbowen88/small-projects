3232b7dab1d2445c9fbd73c6f582a5d5a1e36a9b
sbowen88 <sbowey@gmail.com>
2018-08-15 19:33:13 -0700

    toggle

--- jquery_practice.js
+++ jquery_practice.js
@@ -33,3 +33,17 @@ $(document).ready(() => {
     })
   }
   )
+  $(document).ready(() => {
+    $('.hide-button').on('click', () => {
+      $('.first-image').hide();
+    });
+    
+    $('.show-button').on('click', () => {
+      $('.first-image').show();
+    });
+    
+    $('.toggle-button').on('click', () => {
+      $('.first-image').toggle()
+    });
+    
+  });
