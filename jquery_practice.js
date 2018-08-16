6134a338968e92c9231c1b28822eacdbbbc161cc
sbowen88 <sbowey@gmail.com>
2018-08-16 15:55:46 -0700

    chaining events

--- jquery_practice.js
+++ jquery_practice.js
@@ -47,3 +47,24 @@ $(document).ready(() => {
     });
     
   });
+
+  //chaining events together
+  $(document).ready(() => {
+  
+    $('.login-button').on('click', () => {
+      $('.login-form').show();
+    });
+    
+    $('.menu-button').on('mouseenter', () => {
+      $('.nav-menu').show()
+    })
+    
+    $('.nav-menu').on('mouseleave', () => {
+      $('.nav-menu').hide();
+    })
+    
+    $('.product-photo').on('mouseenter', ()=>{
+      $('.product-photo').addClass('photo-active')
+    }).on('mouseleave', ()=>{
+      $('.product-photo').removeClass('photo-active')
+    })
