7244a73e22afcf47d5839776e7261f29b76f9ae7
sbowen88 <sbowey@gmail.com>
2018-08-15 18:14:01 -0700

    dropdown menu functionality

--- jquery_practice.js
+++ jquery_practice.js
@@ -12,3 +12,24 @@ $(document).ready(() => {
   
   
   })
+  $(document).ready(()=>{
+    $('#cart').on('click', ()=>{
+      $('#cartMenu').show()
+    })
+    $('#cart').on('mouseleave', ()=>{
+      $('#cartMenu').hide()
+    })
+    $('#account').on('click', ()=>{
+      $('#accountMenu').show()
+    })
+      $('#account').on('mouseleave', ()=>{
+      $('#accountMenu').hide()
+    })
+    $('#help').on('click', ()=>{
+      $('#helpMenu').show()
+    })
+      $('#help').on('mouseleave', ()=>{
+      $('#helpMenu').hide()
+    })
+  }
+  )
