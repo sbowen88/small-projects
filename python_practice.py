b420d22c04985025d3d28aab6d18a7cdf7465aef
sbowen88 <sbowey@gmail.com>
2018-08-22 13:28:47 -0700

    finished rock paper scissors

--- python_practice.py
+++ python_practice.py
@@ -95,3 +95,25 @@ message = {
   "won": "Yay you won!", 
   "lost": "Aww you lost!"
 }
+
+def decide_winner(user_choice, computer_choice):
+  print "You selected %" % user_choice
+  print "Computer selected: %" % computer_choice
+  if user_choice == computer_choice:
+    print message["tie"]
+  elif user_choice == options[0] and  computer_choice == options[2]:
+    print message["won"]
+  elif user_choice == options[1] and computer_choice == options[0]:
+    print message["won"]
+  elif user_choice == options[2] and computer_choice == options[1]:
+    print message["won"]
+  else:
+    print message["lost"]
+    
+def play_RPS():
+  user_choice = raw_input("Enter Rock, Paper, or Scissors: ")
+  user_choice = user_choice.upper()
+  computer_choice = options[randint(0, 2)]
+  
+  
+play_RPS()
