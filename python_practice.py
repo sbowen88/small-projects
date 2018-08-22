1854adaa6baa092d83ceb204c9e581b21ed741b7
sbowen88 <sbowey@gmail.com>
2018-08-22 15:33:09 -0700

    python challenge

--- python_practice.py
+++ python_practice.py
@@ -118,3 +118,7 @@ def play_RPS():
   
   
 play_RPS()
+
+lloyd = {"name": "Lloyd", "homework": [], "quizzes": [], "tests":[]}
+alice = {"name": "Alice", "homework": [], "quizzes": [], "tests":[]}
+tyler = {"name": "Tyler", "homework": [], "quizzes": [], "tests":[]}
