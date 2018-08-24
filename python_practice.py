f492b8df497bcb7d1b65bbf6b5bd882ef92516e3
sbowen88 <sbowey@gmail.com>
2018-08-24 10:54:56 -0700

    battleship game

--- python_practice.py
+++ python_practice.py
@@ -181,3 +181,27 @@ def get_class_average(class_list):
     results.append(get_average(student))
     
   return average(results)
+
+#Battleship game
+
+from random import randint
+
+board = []
+
+for x in range(0, 5):
+  board.append(["O"] * 5)
+
+def print_board(board):
+  for row in board:
+    print " ".join(row)
+
+def random_row(board):
+  return randint(0, len(board) - 1)
+
+def random_col(board):
+  return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+# Add your code below!
