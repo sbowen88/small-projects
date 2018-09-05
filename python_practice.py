d08dec8c0585306cd28084ad6932fe434ab4c612
sbowen88 <sbowey@gmail.com>
2018-09-04 18:15:08 -0700

    second example

--- python_practice.py
+++ python_practice.py
@@ -272,6 +272,22 @@ while count < 3:
 else:
   print "You win!"
 
+#second example
+from random import randint
+
+# Generates a number from 1 through 10 inclusive
+random_number = randint(1, 10)
+
+guesses_left = 3
+# Start your game!
+guess = int(raw_input("Your guess: "))
+while guesses_left > 0:
+  if guess == random_number:
+    print "You win!"
+    break
+  guesses_left -= 1
+else:
+  print "You lose."
 #Pressure formula
 
 def solution(molar_mass1, molar_mass2, given_mass1, given_mass2, volume, temp) :
