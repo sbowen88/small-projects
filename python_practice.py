1a32bb3eeb9786a7f4ca2fe2dff5647bc49f3e44
sbowen88 <sbowey@gmail.com>
2018-09-05 11:10:22 -0700

    censor functiong

--- python_practice.py
+++ python_practice.py
@@ -347,3 +347,17 @@ def scrabble_score(word):
       if letter == leter:
         total = total + score[leter]
   return total
+
+#censor function
+def censor(text, word):
+    words = text.split()
+    result = ''
+    stars = '*' * len(word)
+    count = 0
+    for i in words:
+        if i == word:
+            words[count] = stars
+        count += 1
+    result =' '.join(words)
+
+    return result
