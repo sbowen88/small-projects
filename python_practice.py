e06ae4464129c10a3e190a677ad74a7c410830b6
sbowen88 <sbowey@gmail.com>
2018-09-04 18:03:50 -0700

    while/else loop

--- python_practice.py
+++ python_practice.py
@@ -255,6 +255,22 @@ while True:
   count += 1
   if count >= 10:
     break
+#while/else
+import random
+
+print "Lucky Numbers! 3 numbers will be generated."
+print "If one of them is a '5', you lose!"
+
+count = 0
+while count < 3:
+  num = random.randint(1, 6)
+  print num
+  if num == 5:
+    print "Sorry, you lose!"
+    break
+  count += 1
+else:
+  print "You win!"
 
 #Pressure formula
 
