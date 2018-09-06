96052f8b2dad946613e9c6b560dad356f02562dc
sbowen88 <sbowey@gmail.com>
2018-09-06 11:40:53 -0700

    even_squares list

--- python_practice.py
+++ python_practice.py
@@ -465,3 +465,13 @@ for key in my_dict:
 #building custom lists
 evens_to_50 = [i for i in range(51) if i % 2 == 0]
 print evens_to_50
+
+#more lists
+doubles_by_3 = [x * 2 for x in range(1, 6) if (x * 2) % 3 == 0]
+print doubles_by_3
+
+# Complete the following line. Use the line above for help.
+even_squares = [x ** 2 for x in range(1,11) 
+if x%2 == 0]
+
+print even_squares
