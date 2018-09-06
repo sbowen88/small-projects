9d306b3ee3cad77330c6d642b863c755fe1ecec3
sbowen88 <sbowey@gmail.com>
2018-09-06 13:37:58 -0700

    more slicing with secret message

--- python_practice.py
+++ python_practice.py
@@ -484,3 +484,8 @@ print cubes_by_four
 squares = [x ** 2 for x in range(1, 11)]
 
 print filter(lambda x: x >= 30 and x <= 70, squares)
+
+#more slicing
+garbled = "!XeXgXaXsXsXeXmX XtXeXrXcXeXsX XeXhXtX XmXaX XI"
+message = garbled[::-2]
+print message
