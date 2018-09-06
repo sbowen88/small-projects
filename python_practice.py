5c069a0e64a60c1eef116cc84a9f696078f37e31
sbowen88 <sbowey@gmail.com>
2018-09-06 11:47:49 -0700

    cubes by four

--- python_practice.py
+++ python_practice.py
@@ -475,3 +475,7 @@ even_squares = [x ** 2 for x in range(1,11)
 if x%2 == 0]
 
 print even_squares
+
+#cubes_by_four
+cubes_by_four = [x ** 3 for x in range(1, 11) if ((x ** 3) % 4) == 0]
+print cubes_by_four
