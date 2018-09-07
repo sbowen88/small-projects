266e456271fd13ff580e7f5c08b09daeba82debd
sbowen88 <sbowey@gmail.com>
2018-09-07 16:21:24 -0700

    more on classes

--- python_practice.py
+++ python_practice.py
@@ -517,3 +517,18 @@ class PartTimeEmployee(Employee):
   def calculate_wage(self, hours):
     self.hours = hours
     return hours * 12.00
+
+#more on classes
+class Car(object):
+  condition = "new"
+  def __init__(self, model, color, mpg):
+    self.model = model
+    self.color = color
+    self.mpg   = mpg
+   
+  def display_car(self):
+    print "This is a %s %s with %s MPG." % (self.color, self.model, str(self.mpg))
+
+my_car = Car("DeLorean", "silver", 88)
+
+my_car.display_car()
