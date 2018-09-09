77c8505de8e128b990092ec6b204e97f95735cae
sbowen88 <sbowey@gmail.com>
2018-09-08 19:47:43 -0700

    java review

---
+++ java_practice.java
@@ -0,0 +1,12 @@
+public class Generalizations {
+	public static void main(String[] args) {
+
+
+		boolean isComplete;
+		int awesomeLevel;
+		int epicLevel;		
+		/*System.out.println(epicLevel);*/
+		
+
+	}
+}
