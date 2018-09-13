0e438ba4d6274c4e1700a438c7cf164b73bcc4c5
sbowen88 <sbowey@gmail.com>
2018-09-12 21:57:33 -0700

    simple foreach

--- first_php.php
+++ first_php.php
@@ -1,4 +1,4 @@
-function g($a, $b) {
+<?php function g($a, $b) {
   echo $a*$b;
 }
 
@@ -6,11 +6,17 @@ $odd_numbers = [1,3,5,7,9];
 unset($odd_numbers[2]); 
 print_r($odd_numbers);
 echo("stuff");
-?>
-
 // for loop 
 $odd_numbers = [1,3,5,7,9];
 for ($i = 0; $i < count($odd_numbers); $i=$i+1) {
     $odd_number = $odd_numbers[$i];
     echo $odd_number . "\n";
 }
+
+// simple foreach
+$even_numbers = [2,4,6,8,10,12,14,16,18,20];
+
+foreach($even_numbers as $even_number){
+echo $even_number . "\n";
+}
+?>
