7fad2b35f40ae2ee53849765e7efb3c0da5be987
sbowen88 <sbowey@gmail.com>
2018-09-12 19:55:05 -0700

    working on php

--- first_php.php
+++ first_php.php
@@ -7,3 +7,10 @@ unset($odd_numbers[2]);
 print_r($odd_numbers);
 echo("stuff");
 ?>
+
+// for loop 
+$odd_numbers = [1,3,5,7,9];
+for ($i = 0; $i < count($odd_numbers); $i=$i+1) {
+    $odd_number = $odd_numbers[$i];
+    echo $odd_number . "\n";
+}
